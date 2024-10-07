48b6ba6d7c67267339e406291671056c12b50650
Subash Pradhan <subashpradhan@mac.home>
2024-10-07 21:10:25 +0200

    Initializing project

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+const Hero: React.FC = () => {
+  return (
+    <>
+      <section className="p-8 text-center bg-blue-500">
+        <h1 className="text-4xl font-bold text-white sm:text-5xl">Welcome to Nylas Demo application</h1>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
